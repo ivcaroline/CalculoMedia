5721a1c45f1b8a82a5f108f4b681e0f8b065f182
Vitoria <vitoria.astolpho@aluno.sesai.br>
2025-06-18 12:15:06 -0300

    Incluindo Nota 3

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function calcularMedia(){
     //Pega os valores dos compos
     var n1= parseFloat(document.getElementById('nota1').value);
     var n2= parseFloat(document.getElementById('nota2').value);
-
-        //Verifica se são números válidos
-    if (isNaN(n1) || isNaN(n2)) {
-        alert("Por favor, insira notas válidas!");
+    var n3= parseFloat(document.getElementById('nota2').value); 
+        
+    //Verifica se são números válidos
+    if (isNaN(n1) || isNaN(n2) || (isNaN(3))){
+    alert("Por favor, insira notas válidas!");
         return;
     }
 
     //Calcular a média
-    var media = (n1+ n2) / 2;
+    var media = (n1+ n2+ n3) / 3;
 
     //Mostra o resultado
     document.getElementById('resultado').innerText = "A média é: " + media.toFixed(2);
